323c2f15220de0a0e663ababcdce593fa53faa5d
dart2112 <DNAblue2112@gmail.com>
2017-03-20 15:39:28 +1030

    Initial commit

---
+++ src/main/java/net/lapismc/lastonline/LastOnline.java
@@ -0,0 +1,19 @@
+package net.lapismc.lastonline;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.ocpsoft.prettytime.PrettyTime;
+
+public final class LastOnline extends JavaPlugin {
+
+    public PrettyTime pt = new PrettyTime();
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
