d00f10b48d9e31bebcd7a9d29ead309f7c7e6d4d
dart2112 <DNAblue2112@gmail.com>
2017-03-20 22:03:28 +1030

    Half working version ready to test

--- src/main/java/net/lapismc/lastonline/LastOnline.java
+++ src/main/java/net/lapismc/lastonline/LastOnline.java
@@ -7,6 +7,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.ocpsoft.prettytime.PrettyTime;
 import org.ocpsoft.prettytime.units.JustNow;
@@ -17,7 +21,7 @@ import java.io.IOException;
 import java.util.*;
 import java.util.logging.Logger;
 
-public final class LastOnline extends JavaPlugin {
+public final class LastOnline extends JavaPlugin implements Listener {
 
     public PrettyTime pt = new PrettyTime(Locale.ENGLISH);
     public File usersFile = new File(getDataFolder(), "users.yml");
@@ -33,6 +37,12 @@ public final class LastOnline extends JavaPlugin {
         pt.removeUnit(JustNow.class);
         pt.removeUnit(Millisecond.class);
         logger.info("LastOnline v." + getDescription().getVersion() + " has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        saveUserMap();
+        logger.info("LastOnline has been disabled!");
     }
 
     public void configs() {
@@ -55,9 +65,53 @@ public final class LastOnline extends JavaPlugin {
         messages = YamlConfiguration.loadConfiguration(messagesFile);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public HashMap<UUID, Long> loadUserMap() {
+        if (userMap == null || userMap.size() == 0) {
+            List<String> usersList = users.getStringList("List");
+            userMap = new HashMap<>();
+            for (String s : usersList) {
+                String[] array = s.split(":");
+                UUID uuid = UUID.fromString(array[0]);
+                Long timestamp = Long.parseLong(array[1]);
+                userMap.put(uuid, timestamp);
+            }
+        }
+        return userMap;
+    }
+
+    public void saveUserMap() {
+        List<String> userList = new ArrayList<>();
+        for (UUID uuid : userMap.keySet()) {
+            Long time = userMap.get(uuid);
+            String userData = uuid.toString() + ":" + time.toString();
+            userList.add(userData);
+        }
+        users.set("List", userList);
+        try {
+            users.save(usersFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        if (getConfig().getBoolean("CountOnlinePlayers")) {
+            enterData(e.getPlayer());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        enterData(e.getPlayer());
+    }
+
+    public void enterData(Player p) {
+        loadUserMap();
+        Date date = new Date();
+        userMap.remove(p.getUniqueId());
+        userMap.put(p.getUniqueId(), date.getTime());
+        saveUserMap();
     }
 
     @Override
@@ -70,18 +124,13 @@ public final class LastOnline extends JavaPlugin {
                     return true;
                 }
             }
-            if (userMap == null || userMap.size() == 0) {
-                List<String> usersList = users.getStringList("List");
-                userMap = new HashMap<>();
-                for (String s : usersList) {
-                    String[] array = s.split(":");
-                    UUID uuid = UUID.fromString(array[0]);
-                    Long timestamp = Long.parseLong(array[1]);
-                    userMap.put(uuid, timestamp);
-                }
-            }
+            loadUserMap();
             if (args.length == 0) {
-                TreeMap<Long, UUID> lastOnline = new TreeMap<>((Comparator<Long>) (o1, o2) -> o2.compareTo(o1));
+                if (userMap.size() == 0) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', messages.getString("Error.NoUsers")));
+                    return true;
+                }
+                TreeMap<Long, UUID> lastOnline = new TreeMap<>((Comparator<Long>) Comparator.reverseOrder());
                 Integer reportingLimit = getConfig().getInt("MaxUsersReporting");
                 for (UUID uuid : userMap.keySet()) {
                     if (lastOnline.size() >= reportingLimit) {
@@ -116,12 +165,17 @@ public final class LastOnline extends JavaPlugin {
                 OfflinePlayer op = Bukkit.getOfflinePlayer(name);
                 UUID uuid = op.getUniqueId();
                 if (userMap.containsKey(uuid)) {
-                    //report time
+                    String timeFormat = pt.format(new Date(userMap.get(uuid)));
+                    String format = messages.getString("SingleReportFormat");
+                    String message = format.replace("%NAME", name).replace("%TIME", timeFormat).replace("%STATUS", op.isOnline() ? "online" : "offline");
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+                } else if (userMap.size() > 0) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', messages.getString("Error.NoSuchUser")));
                 } else {
-                    //send message
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', messages.getString("Error.NoUsers")));
                 }
             } else {
-                //send usage
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', messages.getString("Error.Usage")));
             }
         }
         return true;
