e49678f3d9d8d2d717b754ede1f7fc0cb3cc6eb7
dart2112 <DNAblue2112@gmail.com>
2017-03-20 21:21:54 +1030

    Added most of the command code

--- src/main/java/net/lapismc/lastonline/LastOnline.java
+++ src/main/java/net/lapismc/lastonline/LastOnline.java
@@ -1,19 +1,129 @@
 package net.lapismc.lastonline;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.ocpsoft.prettytime.PrettyTime;
+import org.ocpsoft.prettytime.units.JustNow;
+import org.ocpsoft.prettytime.units.Millisecond;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Logger;
 
 public final class LastOnline extends JavaPlugin {
 
-    public PrettyTime pt = new PrettyTime();
+    public PrettyTime pt = new PrettyTime(Locale.ENGLISH);
+    public File usersFile = new File(getDataFolder(), "users.yml");
+    public File messagesFile = new File(getDataFolder(), "messages.yml");
+    public YamlConfiguration users;
+    public YamlConfiguration messages;
+    public HashMap<UUID, Long> userMap = new HashMap<>();
+    public Logger logger = Bukkit.getLogger();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        configs();
+        pt.removeUnit(JustNow.class);
+        pt.removeUnit(Millisecond.class);
+        logger.info("LastOnline v." + getDescription().getVersion() + " has been enabled!");
+    }
+
+    public void configs() {
+        saveDefaultConfig();
+        if (!usersFile.exists()) {
+            try {
+                usersFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        users = YamlConfiguration.loadConfiguration(usersFile);
+        if (!users.contains("List")) {
+            List<String> list = new ArrayList<>();
+            users.set("List", list);
+        }
+        if (!messagesFile.exists()) {
+            saveResource("messages.yml", true);
+        }
+        messages = YamlConfiguration.loadConfiguration(messagesFile);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("lastonline")) {
+            if (sender instanceof Player) {
+                Player p = (Player) sender;
+                if (!p.hasPermission("lastonline.use")) {
+                    p.sendMessage(ChatColor.RED + "No permission");
+                    return true;
+                }
+            }
+            if (userMap == null || userMap.size() == 0) {
+                List<String> usersList = users.getStringList("List");
+                userMap = new HashMap<>();
+                for (String s : usersList) {
+                    String[] array = s.split(":");
+                    UUID uuid = UUID.fromString(array[0]);
+                    Long timestamp = Long.parseLong(array[1]);
+                    userMap.put(uuid, timestamp);
+                }
+            }
+            if (args.length == 0) {
+                TreeMap<Long, UUID> lastOnline = new TreeMap<>((Comparator<Long>) (o1, o2) -> o2.compareTo(o1));
+                Integer reportingLimit = getConfig().getInt("MaxUsersReporting");
+                for (UUID uuid : userMap.keySet()) {
+                    if (lastOnline.size() >= reportingLimit) {
+                        Long current = userMap.get(uuid);
+                        Long smallest = lastOnline.lastKey();
+                        if (smallest < current) {
+                            lastOnline.remove(smallest);
+                            lastOnline.put(current, uuid);
+                        }
+                    } else {
+                        lastOnline.put(userMap.get(uuid), uuid);
+                    }
+                }
+                StringBuilder sb = new StringBuilder();
+                String format = messages.getString("ListFormat");
+                Integer i = 1;
+                while (!lastOnline.isEmpty()) {
+                    Map.Entry<Long, UUID> entry = lastOnline.firstEntry();
+                    String dateFormat = pt.format(new Date(entry.getKey()));
+                    OfflinePlayer op = Bukkit.getOfflinePlayer(entry.getValue());
+                    String playerName = op.getName();
+                    String line = format.replace("%NAME", playerName).replace("%TIME", dateFormat).replace("%NUMBER", i.toString())
+                            .replace("%STATUS", op.isOnline() ? "online" : "offline");
+                    sb.append(line);
+                    lastOnline.remove(entry.getValue());
+                    i++;
+                }
+                String message = messages.getString("ReportingFormat").replace("%NUMBER", i - 1 + "").replace("%LIST", sb.toString());
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+            } else if (args.length == 1) {
+                String name = args[0];
+                OfflinePlayer op = Bukkit.getOfflinePlayer(name);
+                UUID uuid = op.getUniqueId();
+                if (userMap.containsKey(uuid)) {
+                    //report time
+                } else {
+                    //send message
+                }
+            } else {
+                //send usage
+            }
+        }
+        return true;
     }
 }
